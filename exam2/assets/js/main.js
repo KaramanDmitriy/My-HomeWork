c89b01988eeb6b0202e9a726e78d1a821de0ec1f
KaramanDmitriy <karamandmitriy@gmail.com>
2025-09-03 16:41:35 +0300

    111

--- exam2/assets/js/main.js
+++ exam2/assets/js/main.js
@@ -36,3 +36,40 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scrolled');
   }
 });
+
+
+$(document).ready(function() {
+  const slider = $("#news-slider").lightSlider({
+    item: 3,
+    slideMargin: 30,
+    loop: true,
+    slideMove: 1,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)', 
+    speed: 600,
+    auto: true,
+    pause: 4000,
+    controls: false, // Вимикаємо стандартні стрілки
+    pager: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          item: 1
+        }
+      }
+    ]
+  });
+
+  // Кастомні стрілки
+  $('.news-prev').on('click', function() {
+    slider.goToPrevSlide();
+  });
+
+  $('.news-next').on('click', function() {
+    slider.goToNextSlide();
+  });
+});
+
+
+
+
