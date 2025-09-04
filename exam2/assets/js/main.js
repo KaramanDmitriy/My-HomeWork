53bd9b6aaf6e5ec43213345d93a6e73ac54cc964
KaramanDmitriy <karamandmitriy@gmail.com>
2025-09-04 23:48:15 +0300

    555

--- exam2/assets/js/main.js
+++ exam2/assets/js/main.js
@@ -70,6 +70,14 @@ $(document).ready(function() {
   });
 });
 
+lightGallery(document.getElementById('static-thumbnails'), {
+    animateThumb: false,
+    zoomFromOrigin: false,
+    allowMediaOverlap: true,
+    toggleThumb: true,
+});
+
+
 
 
 
